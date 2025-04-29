c9060ddc202608b799a6c974948b959e12dcfc38
EnetzUni <enetz.q@opendeusto.com>
2025-04-29 08:56:55 +0200

    Crear .c

---
+++ C/pokemon.h
@@ -0,0 +1,40 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+#include "type.h"
+
+#pragma once
+
+typedef struct {
+        /// The Pokedex number for the Pokemon.
+        int id;
+
+        /// The name of the Pokemon.
+        char name[255];
+
+        /// The base hit points of the Pokemon.
+        int hp;
+
+        /// The base attack stat of the Pokemon.
+        int attack;
+
+        /// The base defense stat of the Pokemon.
+        int defense;
+
+        /// The base special attack stat of the Pokemon.
+        int spattack;
+
+        /// The base special defense stat of the Pokemon.
+        int spdefense;
+
+        /// The base speed stat of the Pokemon.
+        int speed;
+
+        /// The list of types of the Pokemon (1 or 2 types)
+        Type type;
+
+        /// The level at which the Pokemon evolves.
+        int evolvl;
+} Pokemon;
+
+#endif
