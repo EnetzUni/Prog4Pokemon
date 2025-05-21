2e9b1e4b5e25eac08476b401690dba31d34255bf
EnetzUni <enetz.q@opendeusto.com>
2025-05-21 10:33:02 +0200

    Add comments

--- C/db.h
+++ C/db.h
@@ -6,14 +6,33 @@
 #include "movement.h"
 #include "player.h"
 
-// Funciones para obtener datos
+// Functions of the Database
+
+// Load a Pokemon using it's id
 Pokemon* loadPokemon(sqlite3*, int);
+
+// Load a PokemonPlayer using it's id
 PokemonPlayer* loadPokemonPlayer(sqlite3*, int);
+
+// Load the movements for a PokemonPlayer, with the id of the PokemonPlayer and an int* to return the count of movements
 Movement** loadPokemonPlayerMovement(sqlite3*, int, int*);
+
+// Check if a Player exists using it's nickname
 int checkPlayer(sqlite3*, char*);
+
+// Check if a Player's exists using it's nickname and password are correct to log in
 int checkPassword(sqlite3*, char*, char*);
+
+// Load a Movement using it's id
 Movement* loadMovement(sqlite3*, int);
+
+// Load a Movement using it's nickname
 Player* loadPlayer(sqlite3*, char*);
-int insertPlayer(sqlite3* db, char* nickname, char* password, bool gender, int maxLVL, int story);
+
+// Load the PokemonPlayer in the team for a Player, with the id of the Player and an int* to return the count of PokemonPlayer
+PokemonPlayer** loadPlayerPokemonPlayer(sqlite3*, int, int*);
+
+// Insert a Player into the database
+int insertPlayer(sqlite3*, Player*);
 
 #endif
